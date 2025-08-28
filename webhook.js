8d00f6281f6fc52a2333b070d0a9a418bad4a14f
Tommmyills <tomyillza@gmail.com>
2025-08-28 22:22:55 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,7 +1,39 @@
-export default function handler(req, res) {
+import { buffer } from "micro";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log("Webhook event received:", req.body);
-    res.status(200).json({ received: true });
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      const buf = await buffer(req);
+      event = stripe.webhooks.constructEvent(
+        buf,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.error("Webhook signature verification failed.", err.message);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the event
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+      console.log("Payment was successful!", session);
+    }
+
+    res.json({ received: true });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
