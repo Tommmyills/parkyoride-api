9e553db373472b509a5ff2b1f54ba58bdfe6560c
Tommmyills <tomyillza@gmail.com>
2025-08-29 08:17:34 +0200

    Update success.js

--- success.js
+++ success.js
@@ -1,10 +1,3 @@
 export default function handler(req, res) {
-if (req.method === "GET") {
-return res.status(200).json({
-message: "Stripe onboarding successful! Your account is now active.",
-});
-} else {
-res.setHeader("Allow", ["GET"]);
-res.status(405).end(`Method ${req.method} Not Allowed`);
-}
+  res.status(200).json({ message: "Stripe onboarding success ✅" });
 }
